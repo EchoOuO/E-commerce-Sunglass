5e33cb01d85e675bf36551bc1c44587d87652033
EchoOuO <window9995@gmail.com>
2024-03-05 10:27:08 -0800

    [Init] Upload all images/icons ; Define premilinary HTML/CSS/JS files

---
+++ README.md
@@ -0,0 +1,3 @@
+A project that showcases how to build a responsive static website for a one-page e-commerce as web developer. This project showcases semantic HTML5, CSS3, Javascript.
+
+Created by Joe, Evelyn, Joao, Matheus
---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* 1. Please divide codes of sections by */
+/* --- section naming & purpose--- */
+/* for example */
+/* --- product display & scroll button --- */
+/* for readable codes */
+
+/* 2. Please name the varaibles or function with meaning naming, make it readable */
+
+/* 3. It's fine to create another js file for new feature */
+
+/* Have Fun LOL */
