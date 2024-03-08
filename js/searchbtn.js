52f3972367e8408932621a0e07edbcad0239426e
joao <melonsfort@gmail.com>
2024-03-08 04:06:26 -0300

    Finishing the navbar

---
+++ js/searchbtn.js
@@ -0,0 +1,4 @@
+// Adicionando o jQuery do Google CDN
+const script = document.createElement('script');
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+document.head.appendChild(script);
