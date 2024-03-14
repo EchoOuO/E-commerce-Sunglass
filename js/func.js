d149fa2325e124ad0a94f2bed0fbd0fb656a6969
joao <melonsfort@gmail.com>
2024-03-14 20:13:00 -0300

    last commit

--- js/func.js
+++ js/func.js
@@ -3,15 +3,11 @@ const prev = document.querySelectorAll(".img-gallery-button");
 prev[0].addEventListener("click", function() {
     $(".img-gallery-container").animate({
         scrollLeft: "-=400"
-    }, 500); // Altere o valor '500' para controlar a velocidade da animação
+    }, 500); 
 });
 
 prev[1].addEventListener("click", function() {
     $(".img-gallery-container").animate({
         scrollLeft: "+=400"
-    }, 500); // Altere o valor '500' para controlar a velocidade da animação
+    }, 500);
 });
-
-function showSlides(e) {
-    // Restante do seu código aqui...
-}
